c14612c8de3e181e8faef0f643d0bb198762240a
CodingCatQAQ <962221345@qq.com>
2024-03-09 05:25:45 +0800

    moba game

---
+++ MOBA GAME/Assets/Game/Scripts/GameInit.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// 游戏初始化脚本
+/// </summary>
+public class GameInit : MonoBehaviour
+{
+    void Start()
+    {
+        //加载登录UI
+        UIManager.Instance.ShowUIPanel(UIDefinit.UIAccount);
+    }
+}
