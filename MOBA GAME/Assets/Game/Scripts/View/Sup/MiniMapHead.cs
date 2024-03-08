c6b8e4a30b87bc175de9b5d851058937a4833dae
CodingCatQAQ <962221345@qq.com>
2024-03-09 06:16:34 +0800

    game

---
+++ MOBA GAME/Assets/Game/Scripts/View/Sup/MiniMapHead.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MiniMapHead : MonoBehaviour
+{
+    void Update()
+    {
+        transform.rotation = Quaternion.Euler(new Vector3(90f, 0f, 0f));
+    }
+}
